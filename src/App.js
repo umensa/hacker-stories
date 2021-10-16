2d166d679af6626e36b17e406db2925780b12cb4
umensa <mensae@posteo.org>
2021-10-16 13:26:35 +0100

    Conclude Lists in React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import * as React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
+
+      <hr />
+
+      <ul>
+        {/* render the list here */}
+        {list.map(function (item) {
+          return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.url}</a>
+            </span>
+            <span> {item.author}</span>
+            <span> {item.num_comments}</span>
+            <span> {item.points}</span>
+          </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }

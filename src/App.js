9ae0532745c339b5bd026f237e711d0cd8edfb24
Mohamed Zouari <mensae@posteo.org>
2021-10-23 20:03:44 +0100

    Meet another React Component

--- src/App.js
+++ src/App.js
@@ -24,27 +24,38 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {/* render the list here */}
-        {list.map(function (item) {
-          return (
+      <List />
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item){
+        return (
           <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.url}</a>
-            </span>
+            <span><a href={item.url}>{item.title}</a></span>
             <span> {item.author}</span>
             <span> {item.num_comments}</span>
             <span> {item.points}</span>
           </li>
-          );
-        })}
-      </ul>
-    </div>
+        );
+      })}
+    </ul>
   );
 }
 

b75c5267191e07a31672a2dac0ba2f44f53b862f
Mohamed Zouari <mensae@posteo.org>
2021-11-16 19:00:38 +0100

    Conclude Props Handling (Advanced)

--- src/App.js
+++ src/App.js
@@ -57,18 +57,18 @@ const Search = ({ search, onSearch }) => (
 
 const List = ({ list }) => (
     <ul>
-      {list.map(({ objectID, ...item }) => (
-        <Item key={objectID} {...item} />
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} />
       ))}
     </ul>
   );
 
-const Item = ({ title, url, author, num_comments, points }) => (
+const Item = ({ item }) => (
   <li>
-    <span><a href={url}>{title}</a></span>
-    <span> {author}</span>
-    <span> {num_comments}</span>
-    <span> {points}</span>
+    <span><a href={item.url}>{item.title}</a></span>
+    <span> {item.author}</span>
+    <span> {item.num_comments}</span>
+    <span> {item.points}</span>
   </li>
 );
 

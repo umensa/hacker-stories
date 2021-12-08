6078c35d3faa42bba757c4948abe1464a208ddcd
umensa <mensae@posteo.org>
2021-12-08 21:48:22 +0100

    Conclude Inline Handler in JSX

--- src/App.js
+++ src/App.js
@@ -13,34 +13,44 @@ const useSemiPersistantState = function(key, initialState) {
   return [value, setValue]
 }
 
-const App = function() {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ]
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
+const App = function() {
   const [searchTerm, setSearchTerm] = useSemiPersistantState('search', 'React')
+
+  const [stories, setStories] = useState(initialStories)
+
+  const handleRemoveStory = function(item) {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    )
+
+    setStories(newStories)
+  }
 
   const handleSearch = (event) => (
     setSearchTerm(event.target.value)
   )
 
-  const searchedStories = stories.filter((story) => 
-   story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  const searchedStories = stories.filter(
+    (story) => story.title.toLowerCase().includes(searchTerm.toLowerCase())
   )
 
   return (
@@ -58,7 +68,7 @@ const App = function() {
       </InputWithLabel>
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   )
 }
@@ -81,21 +91,28 @@ const InputWithLabel = function({type="text", id, value, onInputChange, isFocuse
   )
 }
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
     ))}
   </ul>
 )
 
-const Item = ({ item }) => (
-  <li>
-    <span><a href={item.url}>{item.title}</a></span>
-    <span> {item.author}</span>
-    <span> {item.num_comments}</span>
-    <span> {item.points}</span>
-  </li>
-)
+const Item = function({ item, onRemoveItem }) {
+  return (
+    <li>
+      <span><a href={item.url}>{item.title}</a></span>
+      <span> {item.author}</span>
+      <span> {item.num_comments}</span>
+      <span> {item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)} >
+          Dismiss
+        </button>
+      </span>
+    </li>
+  )
+}
 
 export default App
